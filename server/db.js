a19fc14485afcb43b404775908cee79af6f5df02
guyius <guyb@wix.com>
2017-03-23 15:46:04 +0200

    fixed typo in db field

--- server/db.js
+++ server/db.js
@@ -1,13 +1,9 @@
-const fs = require('fs');
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const dbUrl = `mongodb://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@ds039088.mlab.com:39088/guyius`;
 
 module.exports = {
     writeHashtageToDB: (hashTagsForDB) => {
-        /*fs.writeFile('lalala.json', JSON.stringify(hashTagsForDB), (err, result) => {
-            console.log('success');
-        });*/
         MongoClient.connect(dbUrl, (err, db) => {
             if (err) {
                 console.log('db failed: ' + err);
@@ -16,10 +12,8 @@ module.exports = {
             }
             const collection = db.collection('hashtags');
             for (let hasttag in hashTagsForDB) {
-                console.log(hashTagsForDB[hasttag].value);
-                console.log(hashTagsForDB[hasttag].counter);
                 collection.update(
-                    { hasttag: hashTagsForDB[hasttag].value },
+                    { hashtag: hashTagsForDB[hasttag].value },
                     {
                         $inc: { counter: hashTagsForDB[hasttag].counter },
                         $currentDate: { lastUpdate: true }
